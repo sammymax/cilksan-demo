8a65a2e92556c50650b48e59c1f8d8739465099d
Phil Sun <sammymax@users.noreply.github.com>
2018-03-20 19:45:15 +0000

    Race definitely exists; cilksan table is null though

--- demo.cpp
+++ demo.cpp
@@ -1,17 +1,20 @@
 #include <iostream>
 #include <cilk/cilk.h>
-#include <cilk/cilk_api.h>
+//#include <cilk/cilk_api.h>
 
-void increment(int* x) {
-  *x ^= 3 - 2 * (*x);
+void increment(int *x, int n) {
+  for (int i = 0; i < n; i++)
+    x[i]++;
 }
 
-int main() {
-  int *x = (int*)malloc(sizeof(int));
-  *x = 0;
+int main(int argc, char** argv) {
+  int n = 1;
+  if (argc == 2) n = atoi(argv[1]);
 
-  std::cout << *x << '\n';
-  increment(x);
-  increment(x);
-  std::cout << *x << '\n';
+  int *x = (int*)malloc(n * sizeof(int));
+
+  cilk_for (int i = 0; i < 1000; i++)
+    increment(x, n);
+
+  std::cout << x[0] << '\n';
 }
